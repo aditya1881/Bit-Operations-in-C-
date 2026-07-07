611e7366b39b408fee53975ef2ed651d49bc61e7
Aditya <adityahirade2019@gmail.com>
2026-07-07 12:14:23 +0530

    Check LSB

---
+++ 01_checklsb.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num & 1) {
+        printf("%d LSB is set 1.\n", num);
+    } else {
+        printf("%d is not set 1.\n", num);
+    }
+
+    return 0;
+}
